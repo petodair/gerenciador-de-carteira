d28082d0a332720a4e7c5782468a47d7fc842b99
Peterson Odair <petersomodair@gmail.com>
2024-02-20 10:47:20 -0300

    adicionando ao github

---
+++ gerenciaCarteira/src/test/java/br/com/gerenciaCarteira/GerenciaCarteiraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gerenciaCarteira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciaCarteiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
